3261ad79f44e95134d5fd1a7fb01361799f8552e
Ebanksc27 <126507556+Ebanksc27@users.noreply.github.com>
2023-04-06 14:28:54 -0400

    Add files via upload

---
+++ letconstexer.js
@@ -0,0 +1,19 @@
+const PI = 3.14;
+PI = 42; // Error: Assignment to constant variable.
+
+// Difference between var and let? 
+// var is function scoped and let is block scoped.
+// var is hoisted and let is not.
+// var can be redeclared and let cannot be redeclared.
+
+// What is the difference between var and const? 
+// var is function scoped and const is block scoped.
+// var can be redeclared and reassigned and const cannot be redeclared or reassigned.
+// var is hoisted and const is not.
+
+// What is the difference between let and const?
+// let can be reassigned and const cannot be reassigned.
+
+// What is hoisting?
+// Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution.
+
